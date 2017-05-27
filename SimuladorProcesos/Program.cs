917a0aebd55168607d6d66d005d0a56e00311a59
Ing. Jorge Perales Díaz <slipknot_jpd@hotmail.com>
2017-05-26 21:37:34 -0500

    Process Simulator

--- SimuladorProcesos/Program.cs
+++ SimuladorProcesos/Program.cs
@@ -10,6 +10,44 @@ namespace SimuladorProcesos
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Ingresa el numero de procesos");
+            int Process = Convert.ToInt32(Console.ReadLine());
+            List<int> list = new List<int>();
+            for (int i = 0; i < Process; i++)
+            {
+                Console.WriteLine("Ingresa el numero de ciclos para el proceso " + (i + 1));
+                list.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            Console.WriteLine("Iniciar procesos");
+            Console.ReadKey();
+            for (int i = 0; i < Process; i++)
+            {
+                Console.WriteLine("Se ha ejecutado el ciclo " + (i + 1));
+                for (int j = 0; j < list.Count; j++)
+                {
+                    Random();
+                }
+                int rtemp = Ram();
+                Console.WriteLine("El indice de la memoria es : " + rtemp);
+                Console.WriteLine();
+                rtemp += 1;
+            }
+            Console.ReadLine();
+        }
+
+        public static void Random()
+        {
+            int seed = unchecked(DateTime.Now.Ticks.GetHashCode());
+            Random r = new Random(seed);
+            Console.WriteLine(r.Next());
+        }
+        public static int Ram()
+        {
+            int seed = unchecked(DateTime.Now.Ticks.GetHashCode());
+            Random r = new Random(seed);
+            Console.WriteLine(r.Next(1, 1025));
+            return r.Next(1, 1025);
         }
     }
 }
